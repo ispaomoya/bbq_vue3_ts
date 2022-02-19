2e44fd958bff4a3f9dd72fcf5d63668ca7bc88c4
xiaoyuan <zhy6251@outlook>
2022-02-19 23:09:39 +0800

    feat:新增1.引入vant，让ts支持vant，2.引入less，配置less

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import utils from '@/utils'
+import utils from '@/utils/index'
+import '@/style/style.less'
+// 按需引入vant
+import { Button,Dialog,Toast } from 'vant'
 const app = createApp(App);
+app.use(Button)
+app.use(Dialog)
+app.use(Toast)
 
 // 挂载原型
 app.config.globalProperties.$utils = utils;
-//
+app.config.globalProperties.$Dialog = Dialog;
+app.config.globalProperties.$Toast = Toast;
 
 //路由
 router.beforeEach((to, from, next) => {
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,43 @@
 const { defineConfig } = require('@vue/cli-service')
+const { merge } = require("webpack-merge");
+const tsImportPluginFactory = require("ts-import-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,//关闭eslint
+  chainWebpack: config => {
+    // 让ts支持vant
+    config.module
+      .rule("ts")
+      .use("ts-loader")
+      .tap(options => {
+        options = merge(options, {
+          transpileOnly: true,
+          getCustomTransformers: () => ({
+            before: [
+              tsImportPluginFactory({
+                libraryName: "vant",
+                libraryDirectory: "es",
+                style: true
+              })
+            ]
+          }),
+          compilerOptions: {
+            module: "es2015"
+          }
+        });
+        return options;
+      });
+    // 引入less
+    const oneOfsMap = config.module.rule("less").oneOfs.store;
+    oneOfsMap.forEach(item => {
+      item
+        .use("style-resources-loader")
+        .loader("style-resources-loader")
+        .options({
+          // or an array : ["./path/to/vars.less", "./path/to/mixins.less"] 这里的路径不能使用@，否则会报错
+          patterns: "./src/style/style.less"
+        })
+        .end()
+    })
+  },
 })
