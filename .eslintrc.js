8bbbfa96d095c504ed3e375247672ff97bfce92b
xiaoyuan <zhy6251@outlook>
2022-02-19 14:49:21 +0800

    fix:卸载prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,27 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-        'vue/setup-compiler-macros': true,
-    },
-    extends: [
-        'plugin:vue/essential',
-        'eslint:recommended',
-        '@vue/typescript/recommended',
-        '@vue/prettier',
-        '@vue/prettier/@typescript-eslint',
-        'plugin:vue/vue3-recommended',
-        'plugin:prettier/recommended',
-    ],
-    parserOptions: {
-        parser: 'espree',
-        ecmaVersion: 2022,
-        sourceType: 'module',
-    },
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        // 'vue/script-setup-uses-vars': 'error',
-        // 'no-unused-vars': 'off',
-        // '@typescript-eslint/no-unused-vars': ['error'],
-    },
+  root: true,
+  env: {
+    node: true,
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@typescript-eslint',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    parser: 'espree',
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    // 'vue/script-setup-uses-vars': 'error',
+    // 'no-unused-vars': 'off',
+    // '@typescript-eslint/no-unused-vars': ['error'],
+  },
 }
