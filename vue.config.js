d998d577e430c8f3f3c647f69624fea5378e0ff8
xiaoyuan <zhy6251@outlook>
2022-03-09 23:40:51 +0800

    fix:更改一些配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
-const { merge } = require("webpack-merge");
-const tsImportPluginFactory = require("ts-import-plugin");
+const { merge } = require("webpack-merge")
+const tsImportPluginFactory = require("ts-import-plugin")
+const path = require('path')
+const resolve = (dir) => path.join(__dirname, dir);
 module.exports = defineConfig({
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建
+  productionSourceMap: false,
   transpileDependencies: true,
+  // lintOnSave: process.env.NODE_ENV === 'development',
   lintOnSave: false,//关闭eslint
   chainWebpack: config => {
     // 让ts支持vant
@@ -38,6 +46,8 @@ module.exports = defineConfig({
           patterns: "./src/style/style.less"
         })
         .end()
-    })
+    });
+    config.resolve.alias // 添加别名
+      .set('@', resolve('src'))
   },
 })
