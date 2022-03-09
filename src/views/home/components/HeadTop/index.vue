7541e5a18508921088abdc3a5cff28dff27999a7
xiaoyuan <zhy6251@outlook>
2022-03-09 23:48:40 +0800

    feat:新增首页跳转me页

---
+++ src/views/home/components/HeadTop/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="head-top">
+    <div class="me" @click="toMeClick()">
+      <img src="@/assets/images/boy.png" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const toMeClick = () => {
+  router.push('/me')
+}
+</script>
+
+<style lang="less" scoped>
+.head-top {
+  .me {
+    img {
+      width: 40px;
+      height: 40px;
+    }
+  }
+}
+</style>
