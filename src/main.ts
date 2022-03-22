804bc1346981c5ca13eff52a6fab7e375b2624ba
xiaoyuan <zhy6251@outlook>
2022-03-22 11:22:21 +0800

    feat:新增md5，sha1加密插件

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import utils from '@/utils/index'
+import axios from 'axios'
+import md5 from 'blueimp-md5'
+import sha1 from 'sha1'
 import '@/style/style.less'
 // 按需引入vant
 import { Button,Dialog,Toast } from 'vant'
@@ -15,6 +18,9 @@ app.use(Toast)
 app.config.globalProperties.$utils = utils;
 app.config.globalProperties.$Dialog = Dialog;
 app.config.globalProperties.$Toast = Toast;
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$md5 = md5;
+app.config.globalProperties.$sha1 = sha1;
 
 // //路由
 // router.beforeEach((to, from, next) => {
