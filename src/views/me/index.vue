e27216fe1601eacf7d88d6fb34af6e697e301d5e
xiaoyuan <zhy6251@outlook>
2022-03-22 11:25:05 +0800

    feat:【我的】控制登录弹窗

--- src/views/me/index.vue
+++ src/views/me/index.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="me">我的</div>
+  <div class="me">
+    我的
+    <van-button type="primary" @click="signInClick()">登录</van-button>
+    <LogIn :isShowLogIn="isShowLogIn" @noShowLogIn="noShowLogIn"></LogIn>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { useRoute, useRouter } from 'vue-router';
+import { ref } from 'vue';
+import LogIn from '@/components/Login/index.vue'
+const route = useRoute()
+const router = useRouter()
+let isShowLogIn = ref(false)
+const signInClick = () => {
+  isShowLogIn.value = true
+}
+const noShowLogIn = (value: any) => {
+  isShowLogIn.value = value
+}
 </script>
 
 <style lang="less" scoped>
